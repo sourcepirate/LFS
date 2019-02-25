a9879a268cbfcbd8ba6214fe83217345455bff26
sourcepirate <sathyanarrayanan@yandex.com>
2019-02-25 23:36:43 +0800

    updated inital plate

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+//! The tools provides the implementation for running the
+//! json rpc based compression archive store for text data
+//! the module uses the LZMA/Brotli compression to store text
+//! data
+
+extern crate env_logger;
+extern crate jsonrpc_core;
+extern crate libc;
+extern crate lzma;
+extern crate serde;
+
+#[macro_use]
+extern crate log;
+
+#[macro_use]
+extern crate jsonrpc_derive;
+
+#[macro_use]
+extern crate serde_derive;
+
+pub mod rpc;
+
+fn main() {
+    env_logger::init();
+}
